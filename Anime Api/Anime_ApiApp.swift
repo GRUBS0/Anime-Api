d466d4dd62eafb48e46654831d4f3b3769dc4325
Joseph <jjphillipse4@gmail.com>
2026-03-10 10:26:52 -0500

    Initial Commit

---
+++ Anime Api/Anime_ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_ApiApp.swift
+//  Anime Api
+//
+//  Created by joseph phillips on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Anime_ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
